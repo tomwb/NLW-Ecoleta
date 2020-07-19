bd0ee8f45b1edfb2beb981dae4daafdfb2fb50cf
welton beck <welton@mbp-de-welton.home>
2020-07-19 01:40:06 -0300

    aula 2

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # NLW-Ecoleta
 projeto feito durante a next level week
+
+
+### pastas
+
+  - `server` contem a api em nodeJS
+  - `web` contem o frontend em reactJS
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,40 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('listagem de usuarios');
+app.use(express.json());
 
-  response.json([
-    'Diego',
-    'robson',
-    'rodrigo',
-    'welton'
-  ]);
+const users = [
+  'Diego',
+  'robson',
+  'cleiton',
+  'welton'
+];
+
+app.get('/users', (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : search
+
+  return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+  const data = request.body;
+
+  const user = {
+    name: data.name,
+    email: data.email
+  }
+
+  return response.json(user);
 });
 
 app.listen(3333);
